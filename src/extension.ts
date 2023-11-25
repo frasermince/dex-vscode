a90f817fb9b35256b0413b6a1fe468112091ea5a
Fraser Mince <frasermince@gmail.com>
2023-11-25 01:04:13 -0600

    Add commands to move between files.

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 import { FileExplorer } from "./provider";
+import * as path from "path";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -35,6 +36,102 @@ export async function activate(context: vscode.ExtensionContext) {
   if (workspaceIsValid) {
     new FileExplorer(context);
   }
+
+  let nextFileCommand = vscode.commands.registerCommand(
+    "dex.nextFile",
+    async () => {
+      const editor = vscode.window.activeTextEditor;
+      if (editor) {
+        console.log("EDITOR", editor);
+        const document = editor.document;
+        const directoryPath = path.dirname(document.fileName);
+        const files = await vscode.workspace.fs.readDirectory(
+          vscode.Uri.file(directoryPath)
+        );
+        const currentIndex = files.findIndex(
+          ([file]) => path.join(directoryPath, file) === document.fileName
+        );
+        const nextFile = files[(currentIndex + 1) % files.length][0];
+        const nextFileUri = vscode.Uri.file(path.join(directoryPath, nextFile));
+        const nextFileWithoutExt = path.parse(nextFileUri.fsPath).name;
+        const parentDirectoryPath = path.dirname(directoryPath);
+        const imageResource = vscode.Uri.file(
+          path.join(parentDirectoryPath, "images", nextFileWithoutExt + ".png")
+        );
+
+        const jsonResource = vscode.Uri.file(
+          path.join(
+            parentDirectoryPath,
+            "json_files",
+            nextFileWithoutExt + ".json"
+          )
+        );
+
+        vscode.commands.executeCommand("vscode.open", imageResource, {
+          viewColumn: vscode.ViewColumn.One,
+        });
+        vscode.window.showTextDocument(jsonResource, {
+          viewColumn: vscode.ViewColumn.Two,
+        });
+      }
+    }
+  );
+  context.subscriptions.push(nextFileCommand);
+
+  let copyAndProgressCommand = vscode.commands.registerCommand(
+    "dex.copyAndProgress",
+    async () => {
+      const editor = vscode.window.activeTextEditor;
+      if (editor) {
+        console.log("EDITOR", editor);
+        const document = editor.document;
+        const directoryPath = path.dirname(document.fileName);
+        const files = await vscode.workspace.fs.readDirectory(
+          vscode.Uri.file(directoryPath)
+        );
+        const currentIndex = files.findIndex(
+          ([file]) => path.join(directoryPath, file) === document.fileName
+        );
+        const nextFile = files[(currentIndex + 1) % files.length][0];
+        const nextFileUri = vscode.Uri.file(path.join(directoryPath, nextFile));
+        const nextFileWithoutExt = path.parse(nextFileUri.fsPath).name;
+        const parentDirectoryPath = path.dirname(directoryPath);
+
+        const prevFileUri = vscode.Uri.file(document.fileName);
+        const prevFileWithoutExt = path.parse(prevFileUri.fsPath).name;
+        const prevJsonResource = vscode.Uri.file(
+          path.join(
+            parentDirectoryPath,
+            "json_files",
+            prevFileWithoutExt + ".json"
+          )
+        );
+        const prevJsonContent = await vscode.workspace.fs.readFile(
+          prevJsonResource
+        );
+        const imageResource = vscode.Uri.file(
+          path.join(parentDirectoryPath, "images", nextFileWithoutExt + ".png")
+        );
+
+        const jsonResource = vscode.Uri.file(
+          path.join(
+            parentDirectoryPath,
+            "json_files",
+            nextFileWithoutExt + ".json"
+          )
+        );
+
+        await vscode.workspace.fs.writeFile(jsonResource, prevJsonContent);
+        vscode.commands.executeCommand("vscode.open", imageResource, {
+          viewColumn: vscode.ViewColumn.One,
+        });
+        vscode.window.showTextDocument(jsonResource, {
+          viewColumn: vscode.ViewColumn.Two,
+        });
+      }
+    }
+  );
+  context.subscriptions.push(nextFileCommand);
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
